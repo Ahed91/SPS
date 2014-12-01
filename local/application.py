6bd714fdde009d4f02cf30230d7e46daf208ac59
Ahed Eid <ahedeid@gmail.com>
2014-12-01 09:31:52 +0200

    update local with database

--- local/application.py
+++ local/application.py
@@ -1,33 +1,225 @@
 import sys
 import cherrypy
 import os.path
+import sqlite3
+import time
 
-states = {
-    'LED1': 'Off',
-    'LED2': 'On',
-    'POT' : 0
+locations = {
+    'A1': 'red',
+    'A2': 'green',
+    'A3': 'red',
+    'A4': 'green',
+    'B1': 'red',
+    'B2': 'green',
+    'B3': 'red',
+    'B4': 'green',
+    'C1': 'red',
+    'C2': 'green',
+    'C3': 'red',
+    'C4': 'green',
+    'D1': 'red',
+    'D2': 'green',
+    'D3': 'red',
+    'D4': 'green',
 }
 
-class States:
+class Root:
 
     exposed = True
 
-    def GET(self, id=None):
-    
+    def GET(self, id=None , *pargs):
+
         if id == None:
-            raise cherrypy.HTTPRedirect('/api/states/index.html')
+            raise cherrypy.HTTPRedirect('/index')
 
+        elif id in locations:
+            return('%s' % (locations[id]))
 
-        elif id in states:
-            return('%s' % (states[id]))
+        elif id == 'createdb':
+            # Get a cursor 
+            db = sqlite3.connect('data.db')
+            cursor = db.cursor()
+            cursor.execute('''
+                       CREATE TABLE IF NOT EXISTS reserve(id INTEGER PRIMARY KEY, username TEXT,location TEXT,
+                                            date TEXT, timefrom TEXT, timeto TEXT , note TEXT)   
+                           ''')
+            db.commit()
+            
+            cursor.execute('''
+                       CREATE TABLE IF NOT EXISTS contact(id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT,
+                                            email TEXT, body TEXT)   
+                           ''')
+            db.commit()
+            db.close()
+            return 'Database Created'
+
+        if not os.path.exists('data.db'):
+            self.GET(id='createdb')
+
+        elif id == 'contact':
+            first_name = pargs[0]
+            last_name = pargs[1]
+            email = pargs[2]
+            message = pargs[3]
+            try:
+                # Connect to database
+                db = sqlite3.connect('data.db')
+                cursor = db.cursor()
+                cursor.execute('''INSERT INTO contact(firstname, lastname, email, message)
+                                             VALUES(?,?,?,?)''', (first_name, last_name, email, message))
+                db.commit()
+            except Exception as e:
+                db.rollback()
+                raise e
+            finally:
+                db.close()
+                id = cursor.lastrowid
+                print('contact inserted', id)
+                return 'Thanks for your interest'
+
+        elif id == 'reserve':
+            username = pargs[0]
+            location = pargs[1]
+            date = pargs[2]
+            timefrom = pargs[3]
+            timeto = pargs[4]
+            note = pargs[5]
+            if int(timefrom.split(':')[0]) > int(timeto.split(':')[0]):
+                return 'Error Time Interval (timefrom hour > timeto hour)'
+            if int(timefrom.split(':')[0]) == int(timeto.split(':')[0]):
+                if int(timefrom.split(':')[1]) >= int(timeto.split(':')[1]):
+                    return 'Error Time Interval (timefrom min > timeto min)'
+            # Connect to database
+            db = sqlite3.connect('data.db')
+            cursor = db.cursor()
+            cursor.execute('''SELECT * FROM reserve where date=?''',(date,) )
+            for i in cursor.fetchall():
+                if location == i[2]:
+                    if int(timefrom.split(':')[0]) == int(i[4].split(':')[0]):
+                        if int(timefrom.split(':')[1]) == int(i[4].split(':')[1]):
+                            return 'Choose another time or location(1timefrom equel old timrfrom)'
+                        if int(timefrom.split(':')[1]) > int(i[4].split(':')[1]):
+                            if int(timefrom.split(':')[0]) == int(i[5].split(':')[0]):
+                                if int(timefrom.split(':')[1]) < int(i[5].split(':')[1]):
+                                    return 'Choose another time or location(1timefrom min less than old timeto)'
+                            if int(timefrom.split(':')[0]) < int(i[5].split(':')[0]):
+                                return 'Choose another time or location(1timefrom hour less than old timeto)'
+                    if int(timefrom.split(':')[0]) < int(i[4].split(':')[0]):
+                        if int(timefrom.split(':')[0]) < int(timeto.split(':')[0]) :
+                            return 'Choose another time or location(2timeto hour more than old timefrom)'
+                    if int(timefrom.split(':')[0]) > int(i[4].split(':')[0]):
+                        if int(timefrom.split(':')[0]) < int(i[5].split(':')[0]):
+                            return 'Choose another time or location(3timefrom hour less than old timeto)'
+                        if int(timefrom.split(':')[0]) == int(i[5].split(':')[0]):
+                            if int(timefrom.split(':')[1]) < int(i[5].split(':')[1]):
+                                return 'Choose another time or location(3timefrom min less than old timeto)'
+
+            cursor.execute('''INSERT INTO reserve(username, location, date, timefrom, timeto, note)
+                                             VALUES(?,?,?,?,?,?)''', (username, location, date, timefrom, timeto, note))
+            db.commit()
+            db.close()
+            id = cursor.lastrowid
+            print('reserve inserted', id)
+            return 'Done'
+
+        if id == 'getallfromdb':
+            db = sqlite3.connect('data.db')
+            cursor = db.cursor()
+            cursor.execute('SELECT * FROM reserve')
+            l = []
+            for _ in cursor.fetchall():
+                l.append(_)
+            db.close()
+            return str(l).strip('[]').replace('),','),</br>')
+
+        if id == 'gettodayfromdb':
+            db = sqlite3.connect('data.db')
+            cursor = db.cursor()
+            cursor.execute('SELECT * FROM reserve where date=?',(time.strftime('%d.%m.%Y'),))
+            l = []
+            for _ in cursor.fetchall():
+                l.append(_)
+            db.close()
+            return str(l).strip('[]').replace('),','),</br>')
+
+        if id == 'getcurrentfromdb':
+            current_date=time.strftime('%d.%m.%Y')
+            if current_date[0]=='0':
+                current_date = current_date[1:]
+            l=[]
+            db = sqlite3.connect('data.db')
+            cursor = db.cursor()
+            cursor.execute('SELECT * FROM reserve where date=?',(current_date,))
+            for _ in cursor.fetchall():
+                l.append(_)
+            db.close()
+            print l
+            current_min = int(time.strftime('%M'))
+            current_hour = int(time.strftime('%H'))
+            close_min = 59
+            close_hour = 23
+            index = -1
+            index2 = -2
+            if len(l):
+                for idx,item in enumerate(l):
+                    if int(item[4].split(':')[0]) == current_hour:
+                        index2 = -2
+                        if int(item[4].split(':')[1]) >= current_min and  int(item[4].split(':')[1]) < close_min :
+                            close_min = int(item[4].split(':')[1])
+                            index = idx
+                if index2 == -1 :
+                    for idx2,item2 in enumerate(l):
+                        if int(item[4].split(':')[0]) > current_hour  and int(item[4].split(':')[0]) < close_hour :
+                            close_hour = int(item[4].split(':')[0])
+                            index2 = idx2
+                    if int(item[4].split(':')[0]) == close_hour :
+                        if int(item[4].split(':')[1]) < close_min :
+                            close_min = int(item[4].split(':')[1])
+                            index = idx
+                return str(l[index])
+            return ''
+
+        if id == 'getcontact':
+            db = sqlite3.connect('data.db')
+            cursor = db.cursor()
+            cursor.execute('SELECT * FROM contact')
+            l = []
+            for _ in cursor.fetchall():
+                l.append(_)
+            db.close()
+            return str(l).strip('[]').replace('),','),</br>')
+        elif id == 'cleardb':
+            # Get a cursor object
+            db = sqlite3.connect('data.db')
+            cursor = db.cursor()
+            cursor.execute('''DROP TABLE reserve''')
+            db.commit()
+            cursor.execute('''DROP TABLE contact''')
+            db.commit()
+            db.close()
+            return 'White Ya Database'
+
+        elif id == 'calls':
+            return str(locations.keys()) , '''</br> createdb </br>
+                       contact </br>
+                       reserve </br>
+                       getallfromdb </br>
+                       gettodayfromdb </br>
+                       getcurrentfromdb </br>
+                       get contact </br>
+                       cleardb </br>
+
+                    '''
+
         else:
             return('The %s is not defined ' % id)
 
-    def PUT(self, led1=None, led2=None, pot=None):
-        states['LED1'] = led1 or states['LED1']
-        states['LED2'] = led2 or states['LED2']
-        states['POT'] = pot or states['POT']
-        return led1 or led2 or pot
+    def PUT(self, location = None, value =None):
+        locations[location] = value or locations[location] 
+        return location,value
+
+    def POST(self,*pargs):
+        return 'none'
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -35,31 +227,41 @@ conf = {'/':
             {'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
             'tools.staticfile.root': current_dir },
 
-        '/index.html':
+        '/index':
             {'tools.staticfile.on': True,
             'tools.staticfile.filename':
             'static/index.html'},
+
+        '/img.html':
+            {'tools.staticfile.on': True,
+            'tools.staticfile.filename':
+            'static/img.html'},
 
         '/style.css':
             {'tools.staticfile.on': True,
             'tools.staticfile.filename':
             'static/style.css'},
 
-        '/jquery.js':
+        '/jquery.min.js':
             {'tools.staticfile.on': True,
             'tools.staticfile.filename':
-            'static/jquery.js'},
+            'static/jquery.min.js'},
 
-        '/jquery-ui.min.js':
+        '/w2ui-1.4.2.min.js':
             {'tools.staticfile.on': True,
             'tools.staticfile.filename':
-            'static/jquery-ui.min.js'},
+            'static/w2ui-1.4.2.min.js'},
+
+        '/w2ui-1.4.2.min.css':
+            {'tools.staticfile.on': True,
+            'tools.staticfile.filename':
+            'static/w2ui-1.4.2.min.css'},
 
         '/script.js':
             {'tools.staticfile.on': True,
             'tools.staticfile.filename':
             'static/script.js'},
-            
+
          '/background.png':
             {'tools.staticfile.on': True,
             'tools.staticfile.filename':
@@ -75,13 +277,14 @@ conf = {'/':
             'tools.staticfile.filename':
             'static/red.png'},
 
+         '/contact.svg':
+            {'tools.staticfile.on': True,
+            'tools.staticfile.filename':
+            'static/contact.svg'},
+
          'global':{
                 'server.socket_port':8080
                 }}
-cherrypy.quickstart(States(), '/api/states',conf                
+cherrypy.quickstart(Root(), '/',conf
     )
-
-
-
-
 
--- local/static/script.js
+++ local/static/script.js
@@ -2,41 +2,295 @@
 function test(){
 alert('ahed')
 }
-
-$(function() {
-    $( document  ).tooltip({
-        position: {
-            my: "center bottom-20",
-    at: "center top",
-    using: function( position, feedback  ) {
-        $( this  ).css( position  );
-        $( "<div>"  )
-        .addClass( "arrow"  )
-        .addClass( feedback.vertical  )
-        .addClass( feedback.horizontal  )
-        .appendTo( this  );
-    }
-        }
-    });
-});
-
-
 $(document).ready(function() {
-	
-	$('#container').fullPaged({
-		'direction' : 'bottom'
-	});
 
 var myInterval = setInterval(function(){
-$.ajax({
-    url:"http://cherrypy-ahed.rhcloud.com/api/states/LED1"
-    }).success (function(responseText) {
-   var array = responseText.split(',')
-   $("#led1").html(array[0]) 
-$("#led2").html(array[1]) 
-$("#pot").html(array[2]) 
-       })
+    $.ajax({
+          url:document.URL + "A1"
+           }).success (function(responseText) {
+           $("#A1").attr('src',responseText+'.png')
+           })
 
-   },1000)
+    $.ajax({
+          url:document.URL + "A2"
+           }).success (function(responseText) {
+           $("#A2").attr('src',responseText+'.png')
+           })
+
+    $.ajax({
+          url:document.URL + "A3"
+           }).success (function(responseText) {
+           $("#A3").attr('src',responseText+'.png')
+           })
+
+    $.ajax({
+          url:document.URL + "A4"
+           }).success (function(responseText) {
+           $("#A4").attr('src',responseText+'.png')
+           })
+
+    $.ajax({
+          url:document.URL + "B1"
+           }).success (function(responseText) {
+           $("#B1").attr('src',responseText+'.png')
+           })
+
+    $.ajax({
+          url:document.URL + "B2"
+           }).success (function(responseText) {
+           $("#B2").attr('src',responseText+'.png')
+           })
+
+    $.ajax({
+          url:document.URL + "B3"
+           }).success (function(responseText) {
+           $("#B3").attr('src',responseText+'.png')
+           })
+
+    $.ajax({
+          url:document.URL + "B4"
+           }).success (function(responseText) {
+           $("#B4").attr('src',responseText+'.png')
+           })
+
+    $.ajax({
+          url:document.URL + "C1"
+           }).success (function(responseText) {
+           $("#C1").attr('src',responseText+'.png')
+           })
+
+    $.ajax({
+          url:document.URL + "C2"
+           }).success (function(responseText) {
+           $("#C2").attr('src',responseText+'.png')
+           })
+
+    $.ajax({
+          url:document.URL + "C3"
+           }).success (function(responseText) {
+           $("#C3").attr('src',responseText+'.png')
+           })
+
+    $.ajax({
+          url:document.URL + "C4"
+           }).success (function(responseText) {
+           $("#C4").attr('src',responseText+'.png')
+           })
+
+    $.ajax({
+          url:document.URL + "D1"
+           }).success (function(responseText) {
+           $("#D1").attr('src',responseText+'.png')
+           })
+
+    $.ajax({
+          url:document.URL + "D2"
+           }).success (function(responseText) {
+           $("#D2").attr('src',responseText+'.png')
+           })
+
+    $.ajax({
+          url:document.URL + "D3"
+           }).success (function(responseText) {
+           $("#D3").attr('src',responseText+'.png')
+           })
+
+    $.ajax({
+          url:document.URL + "D4"
+           }).success (function(responseText) {
+           $("#D4").attr('src',responseText+'.png')
+           })
+
+           },10000)
 });
 
+function contact () {
+    if (!w2ui.contact) {
+        $().w2form({
+            name: 'contact',
+            style: 'border: 0px; background-color: transparent;',
+            formHTML: 
+                '<div class="w2ui-page page-0">'+
+                '    <div class="w2ui-field">'+
+                '        <label>First Name:</label>'+
+                '        <div>'+
+                '           <input name="first_name" type="text" maxlength="100" style="width: 250px"/>'+
+                '        </div>'+
+                '    </div>'+
+                '    <div class="w2ui-field">'+
+                '        <label>Last Name:</label>'+
+                '        <div>'+
+                '            <input name="last_name" type="text" maxlength="100" style="width: 250px"/>'+
+                '        </div>'+
+                '    </div>'+
+                '    <div class="w2ui-field">'+
+                '        <label>Email:</label>'+
+                '        <div>'+
+                '            <input name="email" type="text" style="width: 250px"/>'+
+                '        </div>'+
+                '    </div>'+
+                '    <div class="w2ui-field">'+
+                '        <label>body :</label>'+
+                '        <div>'+
+                '           <textarea name="body" rows="4" style="width: 250px"></textarea>'+
+                '        </div>'+
+                '    </div>'+
+                '</div>'+
+                '<div class="w2ui-buttons">'+
+                '    <button class="btn" name="reset">Reset</button>'+
+                '    <button class="btn" name="save">Save</button>'+
+                '</div>',
+            fields: [
+                { field: 'first_name', type: 'text', required: true },
+                { field: 'last_name', type: 'text', required: true },
+                { field: 'email', type: 'email' },
+                { field: 'body', type: 'text' },
+            ],
+            record: { 
+                first_name    : 'Ahed',
+                last_name     : 'Eid',
+                email         : 'Ahed@email.com',
+                body          : 'sample text sample text sample text sample text sample text sample text sample text'
+            },
+            actions: {
+                "save": function () { 
+                    $.ajax({
+                          url:document.location['origin']+'/contact/'+$('input#first_name').val()+'/'+$('input#last_name').val()+'/'+$('input#email').val()+'/'+$('textarea#body').val()
+                            
+                    }).success (function(responseText) {
+                           alert(responseText)
+                    })
+                },
+                "reset": function () { this.clear(); },
+            }
+        });
+    }
+    $().w2popup('open', {
+        title   : 'Contact Us',
+        body    : '<div id="form" style="width: 100%; height: 100%;"></div>',
+        style   : 'padding: 15px 0px 0px 0px',
+        width   : 500,
+        height  : 300, 
+        showMax : true,
+        onToggle: function (event) {
+            $(w2ui.contact.box).hide();
+            event.onComplete = function () {
+                $(w2ui.contact.box).show();
+                w2ui.contact.resize();
+            }
+        },
+        onOpen: function (event) {
+            event.onComplete = function () {
+                // specifying an onOpen handler instead is equivalent to specifying an onBeforeOpen handler, which would make this code execute too early and hence not deliver.
+                $('#w2ui-popup #form').w2render('contact');
+            }
+        }
+    });
+}
+
+
+function reserve(loc) {
+    if (!w2ui.reserve) {
+        $().w2form({
+            name: 'reserve',
+            style: 'border: 0px; background-color: transparent;',
+            formHTML: 
+                '<div class="w2ui-page page-0">'+
+                '    <div class="w2ui-field">'+
+                '        <label>Name :</label>'+
+                '        <div>'+
+                '           <input name="username" type="text" maxlength="100" style="width: 250px"/>'+
+                '        </div>'+
+                '    </div>'+
+                '    <div class="w2ui-field">'+
+                '        <label>Location:</label>'+
+                '        <div>'+
+                '           <input name="location" type="text" maxlength="2" style="width: 250px"/>'+
+                '        </div>'+
+                '    </div>'+
+                '    <div class="w2ui-field">'+
+                '        <label>Date:</label>'+
+                '        <div>'+
+                '            <input name="date" type="date" style="width: 250px"/>'+
+                '        </div>'+
+                '    </div>'+
+                '    <div class="w2ui-field">'+
+                '        <label>Start Time:</label>'+
+                '        <div>'+
+                '            <input name="timefrom" type="time" style="width: 250px"/>'+
+                '        </div>'+
+                '    </div>'+
+                '    <div class="w2ui-field">'+
+                '        <label>End Time:</label>'+
+                '        <div>'+
+                '            <input name="timeto" type="time" style="width: 250px"/>'+
+                '        </div>'+
+                '    </div>'+
+                '    <div class="w2ui-field">'+
+                '        <label>Note :</label>'+
+                '        <div>'+
+                '           <textarea name="note" rows="4" style="width: 250px"></textarea>'+
+                '        </div>'+
+                '    </div>'+
+                '</div>'+
+                '<div class="w2ui-buttons">'+
+                '    <button class="btn" name="reset">Reset</button>'+
+                '    <button class="btn" name="save">Save</button>'+
+                '</div>',
+            fields: [
+                { field: 'username', type: 'text', required: true },
+                { field: 'location', type: 'text', required: true },
+                { field: 'date', type: 'date',  options  : {
+                                                              format: 'd.m.yyyy',
+                                                              start : new Date().getDate()+'.'+(new Date().getMonth()+1)+'.'+new Date().getFullYear()
+                                                           }
+                },
+                { field: 'timefrom', type: 'time', options: { format: 'h24', start:new Date().getHours()+':00' }},
+                { field: 'timeto', type: 'time', options: { format: 'h24', start:new Date().getHours()+':00' }},
+                { field: 'note', type: 'text' },
+            ],
+            record: { 
+                username : 'Ahed',
+                location : loc,
+                date     : new Date().getDate()+'.'+(new Date().getMonth()+1)+'.'+new Date().getFullYear(),
+                timefrom         : new Date().getHours()+':00',
+                timeto         : new Date().getHours()+':00',
+                note          : 'Anything else'
+            },
+            actions: {
+                "save": function () { 
+                    $.ajax({
+                          url:document.location['origin']+'/reserve/'+$('input#username').val()+'/'+$('input#location').val()+'/'+$('input#date').val()+'/'+$('input#timefrom').val()+'/'+$('input#timeto').val()+'/'+$('textarea#note').val()
+                            
+                    }).success (function(responseText) {
+                           alert(responseText)
+                       })
+                },
+                "reset": function () { this.clear() },
+            },
+        });
+    }
+    $().w2popup('open', {
+        title   : 'Reserve a Location',
+        body    : '<div id="form" style="width: 100%; height: 100%;"></div>',
+        style   : 'padding: 15px 0px 0px 0px',
+        width   : 500,
+        height  : 300, 
+        showMax : true,
+        onToggle: function (event) {
+            $(w2ui.contact.box).hide();
+            event.onComplete = function () {
+                $(w2ui.contact.box).show();
+                w2ui.contact.resize();
+            }
+        },
+        onOpen: function (event) {
+            event.onComplete = function () {
+                // specifying an onOpen handler instead is equivalent to specifying an onBeforeOpen handler, which would make this code execute too early and hence not deliver.
+                $('#w2ui-popup #form').w2render('reserve');
+                w2ui['reserve'].record['location'] = loc; 
+                w2ui['reserve'].refresh();
+            }
+        }
+    });
+}
