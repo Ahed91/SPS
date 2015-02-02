4f71f154841d56ce463f3ddbf3953df7b22b3c92
Ahed Eid <ahedeid@gmail.com>
2015-02-02 23:43:44 +0200

    revert local port 8081 instead of 8080

--- local/application.py
+++ local/application.py
@@ -1111,7 +1111,7 @@ conf = {'/':
             'static/img/cd-icon-close.svg'},
 
          'global':{
-                'server.socket_port':8081,
+                'server.socket_port':8080,
                 'tools.sessions.on': True
                 }}
 
